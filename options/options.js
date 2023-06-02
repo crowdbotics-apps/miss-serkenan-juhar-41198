489fef561359daecb1c945950080b42eb7d75c32
Crowdbotics <team@crowdbotics.com>
2023-06-02 21:09:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "miss-serkenan-juhar-41198",
+  url: "https://miss-serkenan-juhar-41198.botics.co",
+  api: "https://miss-serkenan-juhar-41198.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
